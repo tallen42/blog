f12b0336bf9d7c36bdc3e103196d022202aa78b7
Tyler Allen <tyler@tallen.me>
2025-06-15 20:34:32 +0000

    code looks how i want it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,49 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import fs from "node:fs";
+import expressiveCode from 'astro-expressive-code';
 
 import remarkMath from "remark-math";
+import {remarkReadTime} from "./src/plugins/remark/remarkReadTime.js";
 import rehypeKatex from "rehype-katex";
-
-import macchiato from "./macchiato.json";
-import caddyfile from "./caddyfile.tmLanguage.json";
+import rehypeSlug from "rehype-slug";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 
+const loadIcon = (name) =>
+    fs.readFileSync(new URL(`public/icons/${name}.svg`, import.meta.url), 'utf-8');
+
+const linkIcon = loadIcon('link');
+
 export default defineConfig({
-  prefetch: true,
-  site: "https://blog.kunet.dev",
-  markdown: {
-    shikiConfig: {
-      themes: {
-        light: 'solarized-light',
-        dark: 'solarized-dark'
-      }
+    prefetch: true,
+    site: 'https://blog.kunet.dev',
+    markdown: {
+        remarkPlugins: [remarkMath, remarkReadTime],
+        rehypePlugins: [
+            rehypeKatex,
+            rehypeSlug,
+            [
+                rehypeExternalLinks,
+                {
+                    target: '_blank',
+                    rel: [],
+                },
+            ],
+        ],
     },
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
-  integrations: [
-    tailwind(),
-    mdx({
-    }),
-    sitemap(),
-  ],
+    integrations: [
+        tailwind(),
+        expressiveCode({
+            themes: ['solarized-dark', 'material-theme-lighter'],
+            styleOverrides: {
+
+            }
+        }),
+        mdx(),
+        sitemap(),
+    ],
 });
